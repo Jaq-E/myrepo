81f0837ab3f636c3670d235006484fe6619f7d7c
Jaq-E <jacqulyn.evans@gmail.com>
2021-03-05 16:15:41 +1100

    Worked through ex_2.2 now onto ex2.3 in py4e

--- Python/ex_2.2.py
+++ Python/ex_2.2.py
@@ -8,11 +8,23 @@ Created on Fri Mar  5 15:40:19 2021
 
 # exercise 2.2 from py4e
 
+## In mac
 #command directory for teminal: jacqulynevans/Documents/University/MA_by_research/Rworkingfolder/myrepo/Python
 #cd = command directory and opens to your folder place
+    # you can write cd then start of folder/file and is unique will fill for you
+    # e.g. in opening this folder cd Py *tab* = cd Python
 #ls = list in terminal - will list your files in a folder
 #pwd = where you are currently working in the cd
 # to run a program in terminal type: python3 file_name.py
 # up arrow runs the script in terminal again
 
-print('hello')
+## In windows
+#working in command prompt not terminal
+# cd = pwd in windows - shows you where you are in computer
+# cd also = code for opening command director folders like in mac
+# der = ls - i.e. to list the files or folders in a directory
+
+greet = input('enter your name: ')
+print('Hello',greet)
+
+# to clear screen in mac terminal is command+k
